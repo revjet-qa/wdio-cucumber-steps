6b10bca9cc1edc24262226102c6ca6d479a79892
Alex Krechik <scraych@gmail.com>
2017-11-23 10:22:36 +0100

    Using of non-global http-server

--- cucumber.sh
+++ cucumber.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Run server in background
-http-server ./test/demo-app -p 9000 > /dev/null &
+node_modules/http-server/bin/http-server ./test/demo-app -p 9000 > /dev/null &
 server=$!
 #Run tests
 ./node_modules/.bin/wdio test/wdio.conf.js
