a9e278dfe61fc886d9210c84d496b01d90121d28
Marketionist <marketionist@gmail.com>
2018-03-07 23:51:52 -0400

    Added SPEC=... to set path to .feature file to run tests from

--- cucumber.sh
+++ cucumber.sh
@@ -4,6 +4,11 @@
 node_modules/http-server/bin/http-server ./test/demo-app -p 9000 > /dev/null &
 server=$!
 #Run tests
-./node_modules/.bin/wdio test/wdio.conf.js
+#Use SPEC="..." to set path to .feature file to run tests from
+if [ "$SPEC" == "" ]; then
+    ./node_modules/.bin/wdio test/wdio.conf.js
+else
+    ./node_modules/.bin/wdio test/wdio.conf.js --spec "$SPEC"
+fi
 #Kill server after run
 kill -9 ${server}
