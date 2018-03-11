5fe93cc9b90d52873585e5fdd92de0f663681bfe
Marketionist <marketionist@gmail.com>
2018-03-11 10:54:39 -0300

    Optimized cucumber.sh

--- cucumber.sh
+++ cucumber.sh
@@ -3,13 +3,14 @@
 #Run server in background
 node_modules/http-server/bin/http-server ./test/demo-app -p 9000 > /dev/null &
 server=$!
+
 #Run tests
 #Use SPEC="..." to set path to .feature file to run tests from
-if [ "$SPEC" == "" ]; then
-    ./node_modules/.bin/wdio test/wdio.conf.js
-else
-    ./node_modules/.bin/wdio test/wdio.conf.js --spec "$SPEC"
+if [ "$SPEC" != "" ]; then
+    ADDITIONAL_ARGUMENTS=" --spec $SPEC"
 fi
+
+./node_modules/.bin/wdio test/wdio.conf.js $ADDITIONAL_ARGUMENTS
 
 #Check and save the status of tests run
 if [ $? -eq 0 ]
