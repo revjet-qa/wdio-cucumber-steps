64c2eb1cdcb4fd0961da6c9da1c04e56e0a87613
Marketionist <marketionist@gmail.com>
2018-03-11 10:29:12 -0300

    Fixed cucumber.sh exit status

--- cucumber.sh
+++ cucumber.sh
@@ -10,5 +10,19 @@ if [ "$SPEC" == "" ]; then
 else
     ./node_modules/.bin/wdio test/wdio.conf.js --spec "$SPEC"
 fi
+
+#Check and save the status of tests run
+if [ $? -eq 0 ]
+then
+  echo "Cucumber run success"
+  STATUS=0
+else
+  echo "Cucumber run had errors"
+  STATUS=1
+fi
+
 #Kill server after run
 kill -9 ${server}
+
+#Exit with proper status
+exit $STATUS
