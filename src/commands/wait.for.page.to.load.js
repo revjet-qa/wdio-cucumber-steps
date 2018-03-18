81bbe4e80b510b10b183dce43f8b34d1facb80df
Alex Krechik <scraych@gmail.com>
2018-03-18 16:41:57 +0100

    Using of config finishedLoadingConditions

--- src/commands/wait.for.page.to.load.js
+++ src/commands/wait.for.page.to.load.js
@@ -6,19 +6,10 @@ module.exports = function waitForPageToLoad (callback) {
      * @param {callback} callback - A callback to run
      */
     const timeout = 100;
-    let loaderSelectors = stepsConfig.loaderSelectors;
-
-    let finishedLoadingConditions = function () {
-        // Check if jQuery is present on the page and if any XMLHttpRequests (AJAX) are in progress
-        if (typeof $ !== 'undefined' && $.active) {
-            return false;
-        }
-
-        // Check if any loaders are still present on the page
-        return !loaderSelectors.some((selector) => browser.execute((s) => document.querySelector(s), selector));
-    };
+    const finishedLoadingConditions = stepsConfig.finishedLoadingConditions;
 
     // If loading of the page was finished - launch callback function
+    // TODO - add some waiter here
     if (finishedLoadingConditions()) {
         callback();
     }
