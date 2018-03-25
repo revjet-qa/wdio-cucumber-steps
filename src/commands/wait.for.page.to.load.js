069847e54ba2884782b07f3e6c3d4e0f39b5d8f1
Alex Krechik <scraych@gmail.com>
2018-03-25 16:57:32 +0200

    Fixed waitForPageToLoad function

--- src/commands/wait.for.page.to.load.js
+++ src/commands/wait.for.page.to.load.js
@@ -5,25 +5,5 @@ module.exports = function waitForPageToLoad () {
      * Wait for page to get fully loaded
      * @param {callback} callback - A callback to run
      */
-    return new Promise(function (resolve) {
-        const timeout = 100;
-        const finishedLoadingConditions = stepsConfig.finishedLoadingConditions;
-
-        // If loading of the page was finished - launch callback function
-        // TODO - add some waiter here
-        if (finishedLoadingConditions()) {
-            return resolve();
-        }
-
-        // If loading of the page was not finished - relaunch finishedLoadingConditions() each 100 ms
-        const interval = setInterval(function () {
-            if (finishedLoadingConditions()) {
-                clearInterval(interval);
-                return resolve();
-            }
-        }, timeout);
-    }).catch((err) => {
-        throw new Error(err.message);
-    });
-
+    return browser.waitUntil(stepsConfig.finishedLoadingConditions);
 };
