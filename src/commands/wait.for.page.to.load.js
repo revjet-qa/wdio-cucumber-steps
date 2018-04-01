3bc634576afb00a2ecce1ece91ccb305d29654e0
Marketionist <marketionist@gmail.com>
2018-04-01 10:15:59 -0300

    Added timeoutBeforeWaitForPageToLoad option for slow frameworks

--- src/commands/wait.for.page.to.load.js
+++ src/commands/wait.for.page.to.load.js
@@ -3,7 +3,10 @@
 module.exports = function waitForPageToLoad () {
     /**
      * Wait for page to get fully loaded
-     * @param {callback} callback - A callback to run
      */
-    return browser.waitUntil(stepsConfig.finishedLoadingConditions);
+    return new Promise(function (resolve) {
+        setTimeout(() => {
+            return resolve(browser.waitUntil(stepsConfig.finishedLoadingConditions));
+        }, stepsConfig.timeoutBeforeWaitForPageToLoad);
+    });
 };
