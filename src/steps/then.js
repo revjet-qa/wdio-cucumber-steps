333efeef5a63e0cde50785640eaad1059619d0e8
Marketionist <marketionist@gmail.com>
2018-04-16 07:27:48 -0300

    Added 'I wait for to match' step

--- src/steps/then.js
+++ src/steps/then.js
@@ -1,7 +1,8 @@
 /* eslint new-cap: 0 */
 
 const { defineSupportCode } = require('cucumber');
-const { pageObject, getPageObject } = require('../helpers/objects.processor');
+const { pageObject, getPageObject, dictionaryObject, getDictionaryObject, injectInto } = require(
+    '../helpers/objects.processor');
 const { _r } = require('../helpers/utils');
 
 module.exports = function () {
@@ -30,6 +31,22 @@ module.exports = function () {
 
             try {
                 return await browser.waitForExist(locator, null, true);
+            } catch (err) {
+                throw new Error(err);
+            }
+        });
+
+        Then(_r(`^I wait for ${pageObject} to match ${dictionaryObject}$`), async function async (element, phrase) {
+            /**
+             * The phrase inside the element should match the specified phrase
+             * @param {pageObject} element - String or "page"."object" to select the element
+             * @param {dictionaryObject} phrase - String or "page"."object" with phrase that should be matched
+             */
+            const injection = `contains(.,\"${getDictionaryObject.call(this, phrase)}\")]`;
+            const locator = injectInto(getPageObject(element), injection);
+
+            try {
+                return await browser.waitForExist(locator);
             } catch (err) {
                 throw new Error(err);
             }
