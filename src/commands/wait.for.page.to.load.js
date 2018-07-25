3fc403759549ab009d62c2bef6c9b7acbfb90370
Marketionist <marketionist@gmail.com>
2018-07-24 21:14:09 -0400

    Package deprecated

--- src/commands/wait.for.page.to.load.js
+++
@@ -1,9 +0,0 @@
-/* global stepsConfig */
-
-module.exports = async function waitForPageToLoad () {
-    /**
-     * Wait for page to get fully loaded
-     */
-    await browser.pause(stepsConfig.timeoutBeforeWaitForPageToLoad);
-    return await browser.waitUntil(stepsConfig.finishedLoadingConditions);
-};
